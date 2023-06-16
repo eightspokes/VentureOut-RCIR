64839ca7d701f4148946e0d45335bec7ba120095
romanmuni8 <romanmuni8@gmail.com>
2023-06-16 13:11:48 -0400

    Initial Commit

---
+++ VentureOut-RCIR/VentureOut_RCIRApp.swift
@@ -0,0 +1,17 @@
+//
+//  VentureOut_RCIRApp.swift
+//  VentureOut-RCIR
+//
+//  Created by Roman on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct VentureOut_RCIRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
