c3a8f8ca446a5e0bf7f3d896f1fb9e99d89a2b14
romanmuni8 <romanmuni8@gmail.com>
2023-06-16 18:16:39 -0400

    Added login input components

---
+++ VentureOut-RCIR/Base/Views/InputTextFieldView.swift
@@ -0,0 +1,54 @@
+//
+//  InputTextFieldView.swift
+//  VentureOut-RCIR
+//
+//  Created by Roman on 6/16/23.
+//
+
+import SwiftUI
+
+struct InputTextFieldView: View {
+    
+    @Binding var text: String
+    let placeholder: String
+    let keyboardType: UIKeyboardType
+    let sfSymbol: String?
+    
+    private let textFieldLeading: CGFloat = 30
+        
+    
+    var body: some View {
+        TextField(placeholder, text: $text)
+            .frame(maxWidth: .infinity, minHeight: 44)
+            .padding(.leading, sfSymbol == nil ? textFieldLeading / 2 : textFieldLeading)
+            .keyboardType(keyboardType)
+            .background(
+                ZStack(alignment: .leading){
+                    if let systemImage = sfSymbol {
+                        Image(systemName: systemImage)
+                            .font(.system(size: 16, weight: .semibold))
+                            .padding(.leading, 5)
+                            .foregroundColor(Color.gray.opacity(0.5))
+  
+                    }
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .stroke(Color.gray.opacity(0.25))
+                }
+            )
+    }
+}
+
+struct InputTextFieldView_Previews: PreviewProvider {
+    
+    
+    static var previews: some View {
+        VStack {
+            InputTextFieldView(text: .constant(""), placeholder: "Email", keyboardType: .emailAddress, sfSymbol: "envelope")
+            
+                .preview(with: "Text Input with sfsymbol")
+            
+            InputTextFieldView(text: .constant(""), placeholder: "First Name", keyboardType: .emailAddress, sfSymbol: nil)
+                .preview(with: "Input first name")
+        }
+    }
+}
